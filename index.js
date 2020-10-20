b7be846ee2ed676d244ebf93e4e4f96df5da0d1f
Marcos Alvim <alvim.marcos@gmail.com>
2020-10-20 22:02:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // add solution here
+<<<<<<< HEAD
 function theBeatlesPlay(musicians, instruments) {
   var x =[]
   for (var i = 0; i < musicians.length; i++){
@@ -29,3 +30,12 @@ function iLoveTheBeatles(number) {
 
       return array;
   }
+=======
+function theBeatlesPlay(musicians¸, instruments) {
+  var array[];
+  for(var i=0; i<musicians.length; i++){
+    array.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return array;
+}
+>>>>>>> 72af6a2f60fc487689ebe7e011b5fd074ce2b05e
